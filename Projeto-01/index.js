be2480242579e2a0525f81810d75e6dc638620d8
Ricardo <Ricardoraks1003@gmail.com>
2020-10-09 17:12:51 -0300

    MVC feito com Node.js

--- Projeto-01/index.js
+++ Projeto-01/index.js
@@ -1,33 +1,53 @@
-const express = require("express")
-const app = express()
-const handlebars = require("express-handlebars")
-const bodyParser = require("body-parser")
-const usuarios = require("./models/usuarios")
-const contacts = require("./models/contact")
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const contacts = require("./models/contato/contact");
 
 //template engine
-app.engine("handlebars", handlebars({defaultLayout: "main"}))
-app.set("view engine", "handlebars")
+app.engine("handlebars", handlebars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 
 //body parser
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+//arquivos
 app.use("/assets", express.static('assets'));
 
 //Rotas
 app.get("/", function(req, res){
-	res.render("home");
-})
+	res.render("home/index");
+});
 
 app.get("/contato", function(req, res){
-	res.render("contact");
-})
+	res.render("contato/contact");
+});
 
 app.get("/quem_somos", function(req, res){
-	res.render("quem_somos");
-})
+	res.render("about_us/index");
+});
+
+app.get("/contatos", function(req, res){
+	contacts.findAll({order: [["id", "ASC"]]}).then(function(contacts){
+		res.render("contato/contacts", {contacts: contacts})
+	});
+});
+
+app.get("/contato/:id", function(req, res){
+	contacts.findOne({where: {"id": req.params.id}}).then((contact) =>{
+		res.render("contato/contact_info", {contact: contact})
+	});
+});
+
+app.get("/delete/:id", function(req, res){
+	contacts.destroy({where: {"id": req.params.id}}).then(function(){
+		res.send("<script>alert('Contato removido com sucesso');setTimeout(function() {window.location.href = '/contatos';}, 100);</script>")
+	}).catch(function(erro){
+		res.send("<script>alert('Erro: " + erro + ");setTimeout(function() {window.location.href = '/contatos';}, 100);</script>")
+	});
+});
 
 app.post("/formContact", function(req, res){
 	contacts.create({
@@ -39,20 +59,8 @@ app.post("/formContact", function(req, res){
 		res.send("<script>setTimeout(function() {window.location.href = '/';}, 100);</script>")
 	}).catch(function(erro){
 		res.send("Não foi possivel criar o contato, erro: " + erro)
-	})
-})
-
-app.get("/contatos", function(req, res){
-	contacts.findAll({order: [["id", "ASC"]]}).then(function(contacts){
-		res.render("contacts", {contacts: contacts})
-	})
-})
-
-app.get("/contato/:id", function(req, res){
-	contacts.findOne({where: {"id": req.params.id}}).then((contact) =>{
-		res.render("contact_info", {contact: contact})
-	})
-})
+	});
+});
 
 app.post("/alteracao", function(req, res){
 	contacts.findOne({where: {"id": req.body.id}}).then(function(contact){
@@ -64,19 +72,11 @@ app.post("/alteracao", function(req, res){
 
 		contact.save().then(() => {
 			res.send("<script>setTimeout(function() {window.location.href = '/contatos';}, 100);</script>");
-		})
+		});
 
-	})
-})
-
-app.get("/delete/:id", function(req, res){
-	contacts.destroy({where: {"id": req.params.id}}).then(function(){
-		res.send("<script>alert('Contato removido com sucesso');setTimeout(function() {window.location.href = '/contatos';}, 100);</script>")
-	}).catch(function(erro){
-		res.send("<script>alert('Erro: " + erro + ");setTimeout(function() {window.location.href = '/contatos';}, 100);</script>")
-	})
-})
+	});
+});
 
 app.listen(8081, function(){
 	console.log("Servidor está rodando na url localhost:8081")
-})
+});
